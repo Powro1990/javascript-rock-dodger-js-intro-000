95b0269735176cd3733900be3cef1e696a2ed98f
Paweł Powroźnik <pawel.powroznik21@gmail.com>
2020-01-25 17:10:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,7 +39,9 @@ function checkCollision(rock) {
 
     if (rockLeftEdge <= dodgerLeftEdge && rockLeftEdge >= dodgerLeftEdge) {
       return true
-    } else if(rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge){
+    } else if (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge){
+      return true
+    }else if (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge){
       return true
     }
   }
