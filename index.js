1ab1dcf73808d6672c9d8eee17aa2db2c24e1d14
Paweł Powroźnik <pawel.powroznik21@gmail.com>
2020-01-25 15:25:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -111,7 +111,15 @@ function endGame() {
 }
 
 function moveDodger(e) {
-  // implement me!
+  document.addEventListener('keydown', function(e){
+    if (e.which === LEFT_ARROW){
+      moveDodgerLeft()
+    }
+    
+    
+    
+    
+  })
   /**
    * This function should call `moveDodgerLeft()`
    * if the left arrow is pressed and `moveDodgerRight()`
