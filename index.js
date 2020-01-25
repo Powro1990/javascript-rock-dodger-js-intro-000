6457d08f0f260fdb32157820d18e00e354005957
Paweł Powroźnik <pawel.powroznik21@gmail.com>
2020-01-25 15:34:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -113,11 +113,13 @@ function endGame() {
 function moveDodger(e) {
   
     if (e.which === LEFT_ARROW){
+      e.preventDefault()
       e.stopPropagation()
       moveDodgerLeft()
     }
     
     if (e.which === RIGHT_ARROW){
+      e.preventDefault()
       e.stopPropagation()
       moveDodgerRight()
     }
