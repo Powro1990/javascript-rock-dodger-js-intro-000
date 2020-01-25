eaa4bbb96db34355e756554e455b66da4593a193
Paweł Powroźnik <pawel.powroznik21@gmail.com>
2020-01-25 15:43:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -126,14 +126,6 @@ function moveDodger(e) {
       moveDodgerRight()
       
     }
-    
-  /**
-   * This function should call `moveDodgerLeft()`
-   * if the left arrow is pressed and `moveDodgerRight()`
-   * if the right arrow is pressed. (Check the constants
-   * we've declared for you above.)
-   * And be sure to use the functions declared below!
-   */
 }
 
 function moveDodgerLeft() {
